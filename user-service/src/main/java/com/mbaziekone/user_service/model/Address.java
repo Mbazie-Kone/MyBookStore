7f9532602ea3de20e608a3b3e599fbe26ea89985
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 00:40:59 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
@@ -23,5 +23,15 @@ public class Address implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private String zipCode;
+	
+	private String country;
 
 }
