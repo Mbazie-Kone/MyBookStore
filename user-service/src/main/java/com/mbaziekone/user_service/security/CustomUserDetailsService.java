03a051fb74fae52f733dc148e1afb65dfba8a3b1
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 02:42:34 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/CustomUserDetailsService.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/CustomUserDetailsService.java
@@ -5,16 +5,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.mbaziekone.user_service.model.User;
+import com.mbaziekone.user_service.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
+	private final UserRepository userRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+		return org.springframework.security.core.userdetails.User.builder().username(user.getUsername())
+				.password(user.getPassword()).authorities(user.getRole().getName()).build();
 	}
 
 }
