c66118136ca1f0a788afa9f4d0292484eb89878c
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 20:48:32 +0100

    Updates

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,21 +9,27 @@ import { Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-  registerForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    username: ['', [Validators.required, Validators.minLength(4)]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-    email: ['', [Validators.required, Validators.email]],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zipCode: [''],
-      country: ['']
-    }),
-    roleId:[1] //Default role USER
-  })
+  loginForm: any
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]],
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zipCode: [''],
+        country: ['']
+      }),
+      roleId:[1] //Default role USER
+    });
+  }
 }
