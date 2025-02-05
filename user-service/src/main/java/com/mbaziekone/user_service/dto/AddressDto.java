36ea484df9af35a1e5e9534d986f82cf33085924
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 13:12:34 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/dto/AddressDto.java
+++ user-service/src/main/java/com/mbaziekone/user_service/dto/AddressDto.java
@@ -1,5 +1,17 @@
 package com.mbaziekone.user_service.dto;
 
-public class AddressDto {
+import lombok.Data;
 
+@Data
+public class AddressDto {
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private String zipCode;
+	
+	private String country;
 }
