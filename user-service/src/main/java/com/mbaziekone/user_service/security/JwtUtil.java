80e6001baf2b7767fc5669a9583a8a8a5bc328da
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 01:26:21 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -34,5 +36,33 @@ public class JwtUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(getSigningKey(), SignatureAlgorithm.HS512)
 				.compact();
+	}
+	
+	//Extraction of the Username from the Token
+	public String extractUsername(String token) {
+		
+		return getClaims(token).getSubject();
+	}
+	
+	//Token validation
+	public boolean validateToken(String token) {
+		try {
+			getClaims(token);
+			
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			
+			return false;
+		}
+	}
+	
+	//Claims extraction
+	private Claims getClaims(String token) {
+		
+		return Jwts.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
 	}
 }
