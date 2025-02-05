4e139a0470d0a6bc471a22058131e0fa78d7ccef
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 01:10:04 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
@@ -1,7 +1,12 @@
 package com.mbaziekone.user_service.security;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtil {
@@ -11,4 +16,15 @@ public class JwtUtil {
 	
 	@Value("${jwt.expiration}")
 	private Long expiration;
+	
+	
+	public String generationToken(String username) {
+		
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret)
+				.compact();
+	}
 }
