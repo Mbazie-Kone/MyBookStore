66480362ba94aecab2ac40d2836f3652604b0c2f
M'bazie Kone <mbazie89@gmail.com>
2025-02-05 02:24:05 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
@@ -1,29 +1,51 @@
 package com.mbaziekone.user_service.security;
 
-import java.net.Authenticator;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-	
+
 	private final JwtAuthFilter jwtAuthFilter;
-	
-	private final CustomUserDetailsService customUserDetailsService;
-	
-	
+
+	private final CustomUserDetailsService userDetailsService;
+
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf(CsrfConfigurer::disable)
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("api/auth/**").permitAll().anyRequest().authenticated())
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+
+	}
+	
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
 		
-		return http
-				.csrf().disable()
-				.authorizeHttpRequests( auth -> auth
-						.requestMatchers("api/auth/**").permitAll()
-						.anyRequest().authenticated()
-				)
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder())
+				.and()
+				.build;
+	}
+	
+	public PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
 	}
 }
