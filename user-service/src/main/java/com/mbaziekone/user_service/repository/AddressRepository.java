f60e23b131e6bf82b6f8fe4b72e140d22b6e2e7f
M'bazie Kone <mbazie89@gmail.com>
2025-02-07 19:52:38 +0100

    Updates

---
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.mbaziekone.user_service.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mbaziekone.user_service.model.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+	
+	Optional<Address> findByStreetCityStateZipCodeCountry(String street, String city, String state, String zipCode, String country);
+}
