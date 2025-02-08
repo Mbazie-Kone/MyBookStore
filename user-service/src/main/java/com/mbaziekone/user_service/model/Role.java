433a63a657b0b7fab461622b8fa25aec816d17bc
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:32:49 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/Role.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/Role.java
@@ -1,17 +1,12 @@
 package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,9 +25,7 @@ public class Role implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(unique = true, nullable = false)
-	private String name;
+	@Column(nullable = false, unique = true, length = 50)
+	private String roleName;
 	
-	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<User> users = new ArrayList<>();
 }
