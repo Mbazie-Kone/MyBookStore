775f10aeea9894f7ffa0b2f50fe12cce238b8e5a
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:50:51 +0100

    Updates

---
+++ user-service/src/main/java/com/mbaziekone/user_service/model/UserRole.java
@@ -0,0 +1,33 @@
+package com.mbaziekone.user_service.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRole implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long id; //Managed manually from the database
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id", nullable = false)
+	private Role role;
+}
