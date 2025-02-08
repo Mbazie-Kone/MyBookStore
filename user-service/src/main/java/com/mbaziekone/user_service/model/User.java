227444b7054282aa75e4d16f8d0f547940e65b7e
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 00:01:44 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/User.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable {
 	
 	private String username;
 
-	private String passwordHash;
+	private String password;
 
 	private LocalDateTime createdAt = LocalDateTime.now();
 
