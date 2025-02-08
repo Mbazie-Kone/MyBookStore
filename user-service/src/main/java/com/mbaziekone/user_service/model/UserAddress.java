b40e36aa2dcd34a304cc7df470c54e84ab9c5767
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:54:49 +0100

    Updates

---
+++ user-service/src/main/java/com/mbaziekone/user_service/model/UserAddress.java
@@ -0,0 +1,23 @@
+package com.mbaziekone.user_service.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAddress implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long id; //Managed manually from the database
+}
