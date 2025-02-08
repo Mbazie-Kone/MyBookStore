986296a2c22bfd14ecf244f84eee6cec79b45a62
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:24:40 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/Customer.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/Customer.java
@@ -2,12 +2,16 @@ package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,30 +25,37 @@ import lombok.NoArgsConstructor;
 public class Customer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false, length = 100)
 	private String firstName;
-	
+
 	@Column(nullable = false, length = 100)
 	private String lastName;
-	
+
 	@Column(nullable = false, unique = true, length = 255)
 	private String email;
-	
+
 	@Column(nullable = false, length = 255)
 	private String passwordHash;
-	
+
 	@Column(nullable = false)
 	private LocalDateTime createdAt = LocalDateTime.now();
-	
+
 	@Column(nullable = false)
 	private LocalDateTime updatedAt = LocalDateTime.now();
-	
+
 	@Column
 	private LocalDateTime deledAt;
+
+	@ManyToMany
+	@JoinTable(name = "customer_addresses", 
+		joinColumns = @JoinColumn(name = "customer_id"), 
+		inverseJoinColumns = @JoinColumn(name = "address_id")
+	)
+	private Set<Address> addresses;
 
 }
