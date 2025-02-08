63891fcb78e46b914e99a191b5c018ca68684669
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 00:06:00 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/dto/UserDto.java
+++ user-service/src/main/java/com/mbaziekone/user_service/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.mbaziekone.user_service.dto;
 
+import com.mbaziekone.user_service.model.Role;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -15,5 +17,5 @@ public class UserDto {
 	@Size(min = 8, message = "Password must be at least 6 characters long")
 	private String password;
 	
-	private Long roleId;
+	private Role role;
 }
