b0508ef3591b1dfec372dbe509cc4b3fc01f8df6
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:02:45 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/User.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/User.java
@@ -2,15 +2,16 @@ package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,12 +45,22 @@ public class User implements Serializable {
 	@Column(nullable = false)
 	private LocalDateTime updatedAt = LocalDateTime.now();
 	
-	@ManyToOne
-	@JoinColumn(name = "role_id")
-	private Role role;
+	@Column
+	private LocalDateTime deledAt;
 	
-	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-	@JoinColumn(name = "address_id")
-	private Address address;
+	@ManyToMany
+	@JoinTable(
+			name = "user_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private Set<Role> roles;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "user_addresses",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "address_id"))
+	private Set<Address> addresses;
 
 }
