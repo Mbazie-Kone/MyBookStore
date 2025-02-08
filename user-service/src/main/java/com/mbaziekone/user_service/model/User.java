fe7eae1e8fa94c755d7c0c2eba1c2a68d13709d8
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 08:50:13 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/User.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/User.java
@@ -28,10 +28,6 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String firstName;
-	
-	private String lastName;
-	
 	@Column(unique = true, nullable = false)
 	private String username;
 	
