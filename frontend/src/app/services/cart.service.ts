1485e6501a4117d7b66a28b2571263a0e9ca8f47
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 11:29:26 +0100

    Updates

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
 
+  private cartKey = 'shopping_cart';
+
   constructor() { }
+
+  addToCart(product: any) {
+    let cart = this.getCart();
+    cart.push(product);
+    localStorage.setItem(this.cartKey, JSON.stringify(cart));
+  }
 }
