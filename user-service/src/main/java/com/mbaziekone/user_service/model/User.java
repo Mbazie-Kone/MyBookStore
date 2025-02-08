c5b48f78964d86c6d9fb80fa066b484d148fc2d0
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 00:01:09 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/User.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/User.java
@@ -2,16 +2,13 @@ package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -38,11 +35,8 @@ public class User implements Serializable {
 
 	private LocalDateTime updatedAt = LocalDateTime.now();
 
-	@ManyToMany
-	@JoinTable(name = "user_addresses", 
-		joinColumns = @JoinColumn(name = "user_id"), 
-		inverseJoinColumns = @JoinColumn(name = "address_id")
-	)
+	@ManyToOne
+	@JoinColumn(name = "role_id")
 	private Role role;
 
 }
