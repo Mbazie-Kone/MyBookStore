4de58dc167fd706ff90aee86d7243c6d34904e08
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 10:37:07 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
@@ -27,8 +27,8 @@ public class SecurityConfig {
 		http.csrf(CsrfConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/auth/**", "/api/products", "/api/cart/**").permitAll() //Public endpoints: always accessible
-						.requestMatchers("/api/orders/checkout").authenticated()
-						.requestMatchers("/api/admin/**").hasRole("ADMIN")
+						.requestMatchers("/api/orders/checkout").authenticated() //Endpoints for authenticated users
+						.requestMatchers("/api/admin/**").hasRole("ADMIN") //Endpoints reserved for administrators only
 						.anyRequest().authenticated()
 				)	
 				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
