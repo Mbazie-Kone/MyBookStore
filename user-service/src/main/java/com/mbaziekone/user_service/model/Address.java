b3481e6af53b62e2dcd5e68b8239925f7f9dfd97
M'bazie Kone <mbazie89@gmail.com>
2025-02-08 09:27:01 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
@@ -1,16 +1,11 @@
 package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -39,7 +34,4 @@ public class Address implements Serializable {
 	
 	private String country;
 	
-	@OneToMany(mappedBy = "address", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
-	private List<User> users = new ArrayList<>();
-
 }
