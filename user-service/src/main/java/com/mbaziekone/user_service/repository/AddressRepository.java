7f6f4098c9c0e2d6b71d611129d04cfa392ef52f
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 20:35:52 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/repository/AddressRepository.java
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/AddressRepository.java
@@ -1,12 +1,9 @@
 package com.mbaziekone.user_service.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mbaziekone.user_service.model.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-	
-	Optional<Address> findByStreetAndCityAndStateAndZipCodeAndCountry(String street, String city, String state, String zipCode, String country);
+
 }
--- user-service/src/main/java/com/mbaziekone/user_service/repository/CustomerRepository.java
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.mbaziekone.user_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mbaziekone.user_service.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+	Optional<Customer> findByEmail(String email);
 }
