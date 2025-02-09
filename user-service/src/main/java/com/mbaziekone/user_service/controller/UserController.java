85c59aa5cbdc4bd14aca145e9b44dd14290aa0c7
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 11:25:01 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
+++ user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.mbaziekone.user_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mbaziekone.user_service.model.User;
 
 @RestController
 @RequestMapping("/api/admin")
 public class UserController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/dashboard")
 	@ResponseBody
 	public String helloWorld() {
 		
 		return "Hello World!";
+	}
+	
+	@PostMapping("/login")
+	public User loginAdmin() {
+		
+		return null;
 	}
 }
