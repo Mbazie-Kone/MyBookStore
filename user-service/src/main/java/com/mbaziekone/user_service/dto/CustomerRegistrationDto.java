73886efc8a876f7e345ec89def19e3aed8a22083
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 20:52:32 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/dto/CustomerRegistrationDto.java
+++ user-service/src/main/java/com/mbaziekone/user_service/dto/CustomerRegistrationDto.java
@@ -5,14 +5,32 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class UserDto {
+public class CustomerRegistrationDto {
 	
-	@NotBlank(message = "Username cannot be blank")
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+	
+	@NotBlank(message = "Username is required")
 	@Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
 	private String username;
 	
-	@NotBlank(message = "Password cannot be blank")
+	@NotBlank(message = "Password is required")
 	@Size(min = 8, message = "Password must be at least 6 characters long")
 	private String password;
 	
+	private String phone;
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private String zipCode;
+	
+	private String country;
+
 }
--- user-service/src/main/java/com/mbaziekone/user_service/dto/RoleDto.java
+++
@@ -1,9 +0,0 @@
-package com.mbaziekone.user_service.dto;
-
-import lombok.Data;
-
-@Data
-public class RoleDto {
-	
-	private String roleName;
-}
