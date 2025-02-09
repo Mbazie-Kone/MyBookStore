ec21ebdc9aa49319400f537f902e88655b2f5c0f
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 18:11:04 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/User.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/User.java
@@ -1,20 +1,17 @@
 package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
+import java.util.HashSet;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.FetchType;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,30 +20,21 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User implements Serializable, UserDetails {
+public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id; //Removed @GeneratedValue, as the database will handle auto-increment
 	
 	private String username;
 
 	private String password;
 
-	private LocalDateTime createdAt = LocalDateTime.now();
+	private String email;
 
-	private LocalDateTime updatedAt = LocalDateTime.now();
-
-	@ManyToOne
-	@JoinColumn(name = "role_id")
-	private Role role;
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		
-		return List.of(new SimpleGrantedAuthority(role.getRoleName()));
-	}
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 }
