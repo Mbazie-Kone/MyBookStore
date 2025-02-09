e2fff227dbdce4e070aca9eabc431f0ceb84db1f
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 16:35:13 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/dto/UserDto.java
+++ user-service/src/main/java/com/mbaziekone/user_service/dto/UserDto.java
@@ -9,11 +9,11 @@ import lombok.Data;
 @Data
 public class UserDto {
 	
-	@NotBlank(message = "Username is required")
+	@NotBlank(message = "Username cannot be blank")
 	@Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
 	private String username;
 	
-	@NotBlank(message = "Password is required")
+	@NotBlank(message = "Password cannot be blank")
 	@Size(min = 8, message = "Password must be at least 6 characters long")
 	private String password;
 	
