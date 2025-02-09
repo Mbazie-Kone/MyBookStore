0d1c0b5ecd57ddbc711ca1d7aed002a677e2dcbe
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 11:01:25 +0100

    Updates

---
+++ user-service/src/main/java/com/mbaziekone/user_service/dto/CustomerDto.java
@@ -0,0 +1,16 @@
+package com.mbaziekone.user_service.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+	
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String password;
+
+}
