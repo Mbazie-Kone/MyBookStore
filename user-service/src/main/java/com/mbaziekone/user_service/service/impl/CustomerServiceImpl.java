e382478ba6ce97000bdcef24545dbbb8de020e86
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 21:15:12 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/service/impl/CustomerServiceImpl.java
+++ user-service/src/main/java/com/mbaziekone/user_service/service/impl/CustomerServiceImpl.java
@@ -1,17 +1,38 @@
 package com.mbaziekone.user_service.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mbaziekone.user_service.dto.CustomerRegistrationDto;
 import com.mbaziekone.user_service.model.Customer;
+import com.mbaziekone.user_service.repository.AddressRepository;
+import com.mbaziekone.user_service.repository.CustomerAddressRepository;
+import com.mbaziekone.user_service.repository.CustomerRepository;
 import com.mbaziekone.user_service.service.CustomerService;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private AddressRepository addressRepository;
+	
+	@Autowired
+	private CustomerAddressRepository customerAddressRepository;
+	
 
 	@Override
 	public Customer registerCustomer(CustomerRegistrationDto customerRegistrationDto) {
-		// TODO Auto-generated method stub
+		Customer customer = new Customer();
+		customer.setFirstName(customerRegistrationDto.getFirstName());
+		customer.setLastName(customerRegistrationDto.getLastName());
+		customer.setEmail(customerRegistrationDto.getEmail());
+		//customer.setPassword;
+		customer.setPhone(customerRegistrationDto.getPhone());
+		
+		
 		return null;
 	}
 
