f342b0ad09af3161602b657327bdc003227657d2
M'bazie Kone <mbazie89@gmail.com>
2025-02-09 18:15:58 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
+++ user-service/src/main/java/com/mbaziekone/user_service/model/Address.java
@@ -1,7 +1,6 @@
 package com.mbaziekone.user_service.model;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -31,9 +30,5 @@ public class Address implements Serializable {
 	private String zipCode;
 	
 	private String country;
-	
-	private LocalDateTime createdAt = LocalDateTime.now();
-
-	private LocalDateTime updatedAt = LocalDateTime.now();
 	
 }
