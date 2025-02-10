77f9a1320c5d20d819e42c29195f10b6fb642230
M'bazie Kone <mbazie89@gmail.com>
2025-02-10 07:03:15 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/mbaziekone/user_service/service/impl/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.mbaziekone.user_service.service.impl;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mbaziekone.user_service.model.User;
+import com.mbaziekone.user_service.model.UserRole;
 import com.mbaziekone.user_service.repository.UserRepository;
 import com.mbaziekone.user_service.repository.UserRoleRepository;
 import com.mbaziekone.user_service.service.UserService;
@@ -24,6 +30,10 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+		
+		List<UserRole> userRoles = userRoleRepository.findByUserId(user.getId());
+		Set<SimpleGrantedAuthority> authorities = userRoles.stream().map(ur -> new SimpleGrantedAuthority("ROLE_" + ur.getRole().getName()))
+				.collect(Collectors.toSet());
 		
 		return null;
 	}
