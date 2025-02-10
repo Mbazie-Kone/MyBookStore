86751609cfd45daa040a3cfd2fdda765f96ca23e
M'bazie Kone <mbazie89@gmail.com>
2025-02-10 06:53:50 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/mbaziekone/user_service/service/impl/UserServiceImpl.java
@@ -1,14 +1,23 @@
 package com.mbaziekone.user_service.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.mbaziekone.user_service.repository.UserRepository;
+import com.mbaziekone.user_service.repository.UserRoleRepository;
 import com.mbaziekone.user_service.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private UserRoleRepository userRoleRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
