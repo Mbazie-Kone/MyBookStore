9020733bd2a20b6f64ad6d82b66cf879f9a1bcba
M'bazie Kone <mbazie89@gmail.com>
2025-02-10 06:39:52 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
@@ -16,7 +16,7 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtil {
 	
 	private final String SECRET_KEY = "Y29tcGxleFNlY3JldEtleU9mQW1pbmltdW1MZW5ndGhGQW5kU2FmZUFsd2F5cw==";
-	private final long EXPIRATION_TIME = 86400000;
+	private final long EXPIRATION_TIME = 1000 * 60 * 60; //1 hour
 	private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 	
 	public String generateToken(Authentication authentication) {
