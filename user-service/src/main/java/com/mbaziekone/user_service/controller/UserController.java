51cf36c488cc9b8dad82c8da0d38825ef47c7210
M'bazie Kone <mbazie89@gmail.com>
2025-02-11 02:27:25 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
+++ user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
@@ -1,32 +1,41 @@
 package com.mbaziekone.user_service.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mbaziekone.user_service.model.User;
-import com.mbaziekone.user_service.service.UserService;
+import com.mbaziekone.user_service.security.JwtUtil;
+import com.mbaziekone.user_service.service.impl.UserServiceImpl;
 
 @RestController
 @RequestMapping("/api/admin")
 public class UserController {
 	
 	@Autowired
-	private UserService userService;
+	private AuthenticationManager authenticationManager;
 	
-	@GetMapping("/dashboard")
-	@ResponseBody
-	public String helloWorld() {
-		
-		return "Hello World!";
-	}
+	@Autowired
+	private UserServiceImpl userServiceImpl;
 	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+
 	@PostMapping("/login")
-	public User loginAdmin() {
+	public ResponseEntity<Map<String, String>> login(@RequestParam String username, @RequestParam String password) {
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		UserDetails user = userServiceImpl.loadUserByUsername(username);
+		String token = jwtUtil.generateToken(user.getUsername());
 		
-		return null;
+		return ResponseEntity.ok(Map.of("token", token));
 	}
+	
 }
