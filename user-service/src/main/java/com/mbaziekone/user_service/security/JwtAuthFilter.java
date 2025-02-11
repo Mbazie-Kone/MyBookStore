8d403dfbde447a374bba8735e41f6776ccc6d4c9
M'bazie Kone <mbazie89@gmail.com>
2025-02-11 02:05:23 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtAuthFilter.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtAuthFilter.java
@@ -2,26 +2,29 @@ package com.mbaziekone.user_service.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.mbaziekone.user_service.service.impl.UserServiceImpl;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-
-	private final JwtUtil jwtUtil;
-	private final CustomUserDetailsService userDetailsService;
 	
-	public JwtAuthFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
-		this.jwtUtil = jwtUtil;
-		this.userDetailsService = userDetailsService;
-	}
-
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
