9a4ce07f09cb65b8c0147d3fc9151a7d0d57e5e4
M'bazie Kone <mbazie89@gmail.com>
2025-02-11 20:32:10 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/security/JwtAuthFilter.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/JwtAuthFilter.java
@@ -2,12 +2,14 @@ package com.mbaziekone.api_gateway.security;
 
 import java.net.http.HttpHeaders;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
 import reactor.core.publisher.Mono;
 
+@Component
 public class JwtAuthFilter implements WebFilter{
 
 	@Override
@@ -20,7 +22,9 @@ public class JwtAuthFilter implements WebFilter{
 		}
 		
 		String token = authHeader.substring(7);
-		return null;
+		
+		
+		return chain.filter(exchange);
 	}
 
 }
