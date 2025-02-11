ed4ceeac20d50be1eb098111c2020c13bc2a0f8d
M'bazie Kone <mbazie89@gmail.com>
2025-02-11 02:15:37 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtAuthFilter.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtAuthFilter.java
@@ -3,10 +3,11 @@ package com.mbaziekone.user_service.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.mbaziekone.user_service.service.impl.UserServiceImpl;
@@ -36,16 +37,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 			return;
 		}
 		
-	}
+		String token = authHeader.substring(7);
+		String username = jwtUtil.extractUsername(token);
 		
-	private String parseJwt(HttpServletRequest request) {
-		String headerAuth = request.getHeader("Authorization");
-		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-			
-			return headerAuth.substring(7);
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = userServiceImpl.loadUserByUsername(username);
+			if (jwtUtil.isTokenValid(token, username)) {
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+					userDetails, null, userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+				
+			}
 		}
+		filterChain.doFilter(request, response);
 		
-		return null;
 	}
-
 }
