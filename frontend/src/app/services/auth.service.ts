308227c1f5acb2d012393c2d15857d8dbfbc3ab2
M'bazie Kone <mbazie89@gmail.com>
2025-02-12 13:38:26 +0100

    Updates

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/api/admin/login';
+  private apiUrl = 'http://localhost:8080/api/admin/login'; // Gateway URL
 
   constructor(private http: HttpClient) { }
 
