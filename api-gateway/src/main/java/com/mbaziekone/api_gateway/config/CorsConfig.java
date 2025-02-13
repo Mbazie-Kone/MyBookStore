b116ec034b34b9ba58fb2a8fe57a13940a29c512
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 07:01:18 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/config/CorsConfig.java
@@ -1,5 +1,26 @@
 package com.mbaziekone.api_gateway.config;
 
-public class CorsConfig {
+import java.util.List;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+	
+	public CorsWebFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		
+		config.setAllowedOrigins(List.of("http://localhost:4200"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+		
+		source.registerCorsConfiguration("/**", config);
+		
+		return new CorsWebFilter(source);
+	}
 }
