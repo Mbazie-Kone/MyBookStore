1f96703946b5d171b282514889b64bb5add7518b
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 19:54:50 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.mbaziekone.user_service.security;
 
-import java.net.Authenticator.RequestorType;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 
 import com.mbaziekone.user_service.service.impl.UserServiceImpl;
 
