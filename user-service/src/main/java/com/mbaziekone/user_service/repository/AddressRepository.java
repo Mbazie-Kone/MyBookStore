29aa349e7a62406ca9889a6160b47f41ca2928ee
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 23:14:31 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/repository/AddressRepository.java
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/AddressRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mbaziekone.user_service.model.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+	
 	Optional<Address> findByStreetAndCityAndStateAndZipCodeAndCountry(String street, String city, String state, String zipCode, String country);
 }
--- user-service/src/main/java/com/mbaziekone/user_service/repository/UserRoleRepository.java
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/UserRoleRepository.java
@@ -1,11 +1,9 @@
 package com.mbaziekone.user_service.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mbaziekone.user_service.model.UserRole;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-	List<UserRole> findByUserId(Long userId);
+	
 }
