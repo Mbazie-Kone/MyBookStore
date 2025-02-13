96b3fd1847de3d505af99e2fbd4ea69bb6018835
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 19:41:45 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/config/CorsConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/config/CorsConfig.java
@@ -1,5 +1,17 @@
 package com.mbaziekone.user_service.config;
 
-public class CorsConfig {
+import java.util.List;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+	
+	
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+	}
 }
