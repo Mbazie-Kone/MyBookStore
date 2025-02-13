30564c432fbe8fe54d6e0f416d40745578889e01
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 20:42:05 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
+++ user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,7 @@ public class UserController {
 		return ResponseEntity.ok(new AuthResponse(token));
 	}
 	
+	@GetMapping("/test")
 	public ResponseEntity<String> testAuth() {
 		
 		return ResponseEntity.ok("Authentication ok");
