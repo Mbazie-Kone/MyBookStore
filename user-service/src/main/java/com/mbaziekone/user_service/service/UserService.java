5e5bc7e1e42a225bc797b00dcb181286d85caba0
M'bazie Kone <mbazie89@gmail.com>
2025-02-14 00:35:31 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/service/UserService.java
+++ user-service/src/main/java/com/mbaziekone/user_service/service/UserService.java
@@ -1,19 +1,14 @@
 package com.mbaziekone.user_service.service;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mbaziekone.user_service.model.User;
-import com.mbaziekone.user_service.model.UserRole;
 import com.mbaziekone.user_service.repository.UserRepository;
-import com.mbaziekone.user_service.repository.UserRoleRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,15 +17,16 @@ import lombok.RequiredArgsConstructor;
 public class UserService implements UserDetailsService{
 	
 	private final UserRepository userRepository;
-	private final UserRoleRepository userRoleRepository;
 	
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
 		
-		List<UserRole> userRoles = userRoleRepository.findByUserId(user.getId());
-		Set<SimpleGrantedAuthority> authorities = userRoles.stream().map(ur -> new SimpleGrantedAuthority("ROLE_" + ur.getRole().getName()))
-				.collect(Collectors.toSet());
+		Set<String> roles = userRepository.findRolesByUsername(username);
 		
-		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+		return org.springframework.security.core.userdetails.User.builder()
+			.username(user.getUsername())
+			.password(user.getPassword())
+			.authorities(roles.stream().map(role -> "ROLE_" + role).toArray(String[]::new))
+			.build();
 	}
 }
