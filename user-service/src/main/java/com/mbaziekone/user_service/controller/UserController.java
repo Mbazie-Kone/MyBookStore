a60e1c3edfd1332493a766104c831d21a1ba64f5
M'bazie Kone <mbazie89@gmail.com>
2025-02-13 23:11:30 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
+++ user-service/src/main/java/com/mbaziekone/user_service/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.mbaziekone.user_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- user-service/src/main/java/com/mbaziekone/user_service/repository/CustomerRepository.java
+++ user-service/src/main/java/com/mbaziekone/user_service/repository/CustomerRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mbaziekone.user_service.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
 	Optional<Customer> findByEmail(String email);
 }
