816f7a68d1d9a9025fb29ac31d39c14cff4aecf9
M'bazie Kone <mbazie89@gmail.com>
2025-02-14 01:51:34 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/config/ApiGatewayConfig.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/config/ApiGatewayConfig.java
@@ -1,15 +1,9 @@
 package com.mbaziekone.api_gateway.config;
 
-import java.util.List;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
 
 @Configuration
 public class ApiGatewayConfig {
@@ -23,18 +17,4 @@ public class ApiGatewayConfig {
 				.build();
 	}
 	
-	
-	public CorsWebFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		
-		config.setAllowedOrigins(List.of("http://localhost:4200"));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-		config.setAllowCredentials(true);
-		
-		source.registerCorsConfiguration("/**", config);
-		
-		return new CorsWebFilter(source);
-	}
 }
