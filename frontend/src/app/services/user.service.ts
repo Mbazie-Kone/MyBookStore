79afbb03cdf717ad5b34da6986df6b0e8d770187
M'bazie Kone <mbazie89@gmail.com>
2025-02-16 00:49:22 +0100

    Updates

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = '/api/users/register';
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(username: string, password:string, role: string): Observable<string> {
+    const body = {username, password, role};
+
+    return this.http.post<string>(this.apiUrl, body);
+  }
+}
