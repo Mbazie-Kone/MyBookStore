7d8ce45e9c9488fc61d9c776a4b9d421ed2af9c9
M'bazie Kone <mbazie89@gmail.com>
2025-02-16 03:14:41 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
@@ -1,24 +1,41 @@
 package com.mbaziekone.api_gateway.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-	
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("api/admin/**").hasRole("ADMIN").anyRequest().authenticated());
+
+		return http.build();
+	}
+	
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		
-		return http
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/auth/**").permitAll()
-						.requestMatchers("api/admin/**").hasRole("ADMIN")
-						.anyRequest().authenticated()
-				)
-				.csrf(csrf -> csrf.disable())
-				.cors(cors -> cors.disable())
-				.build();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:4200"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return source;
 	}
 }
