2797637c2b2551776cca1e022623585d225a54cc
M'bazie Kone <mbazie89@gmail.com>
2025-02-16 02:09:34 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
@@ -1,9 +1,22 @@
 package com.mbaziekone.api_gateway.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 	
-	
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		return http
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("api/admin/**").hasRole("ADMIN")
+						.anyRequest().authenticated()
+				)
+				.csrf(csrf -> csrf.disable())
+				.cors(cors -> cors.disable())
+				.build();
+	}
 }
