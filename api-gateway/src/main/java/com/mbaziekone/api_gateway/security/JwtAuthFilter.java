84296f14902a022182ad695004cf5d615350b64e
M'bazie Kone <mbazie89@gmail.com>
2025-02-16 02:15:47 +0100

    Updates

---
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/JwtAuthFilter.java
@@ -0,0 +1,55 @@
+package com.mbaziekone.api_gateway.security;
+
+import java.security.Key;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class JwtAuthFilter implements WebFilter{
+	
+	private static final String SECRET_KEY = "mySecretKeyForJWTAuthenticationInMicroservices";
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return chain.filter(request);
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+            log.info("JWT Validato: Utente = {}", claims.getSubject());
+
+        } catch (Exception e) {
+            log.error("Token JWT non valido!");
+        }
+
+        return chain.filter(request);
+    }
+
+}
--- api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mbaziekone.api_gateway.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,6 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		return http
