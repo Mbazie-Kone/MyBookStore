a7e3dd38945d6a80465f93a7ade5ca7a91d92482
M'bazie Kone <mbazie89@gmail.com>
2025-02-16 01:12:03 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   {path: 'auth/login', component: LoginComponent},
   {path: 'admin/dashboard', component: DashboardComponent, canActivate: [authGuard] },
   {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
-  {path: '**', redirectTo: 'auth/login'}
+  {path: '**', redirectTo: 'auth/login'},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
