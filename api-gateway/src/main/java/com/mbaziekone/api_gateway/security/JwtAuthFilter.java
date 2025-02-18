78dda593322e57fda38cd2fda9afae9c9373cc37
M'bazie Kone <mbazie89@gmail.com>
2025-02-18 01:44:33 +0100

    Updates

--- api-gateway/src/main/java/com/mbaziekone/api_gateway/security/JwtAuthFilter.java
+++ api-gateway/src/main/java/com/mbaziekone/api_gateway/security/JwtAuthFilter.java
@@ -1,30 +1,15 @@
 package com.mbaziekone.api_gateway.security;
 
-import java.security.Key;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Component
-@Slf4j
 public class JwtAuthFilter implements WebFilter {
-	
-	private static final String SECRET_KEY = "mySecretKeyForJWTAuthenticationInMicroservices";
-
-    private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
@@ -32,21 +17,6 @@ public class JwtAuthFilter implements WebFilter {
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             return chain.filter(exchange);
-        }
-
-        String token = authHeader.substring(7);
-
-        try {
-            Claims claims = Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-
-            log.info("JWT Valid: User = {}", claims.getSubject());
-
-        } catch (Exception e) {
-            log.error("Token JWT error!");
         }
 
         return chain.filter(exchange);
