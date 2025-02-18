d934080ac0afa52d8623d33586cfb5c1216495b3
M'bazie Kone <mbazie89@gmail.com>
2025-02-18 03:12:18 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
@@ -16,9 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.server.WebFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mbaziekone.user_service.service.UserService;
 
@@ -31,35 +30,29 @@ public class SecurityConfig {
 
 	private final JwtAuthFilter jwtAuthFilter;
 	private final UserService userService;
-	
+
 	@Bean
-	public WebFilter corsFilter() {
+	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		
-		return new CorsWebFilter(source);
+		return source;
 	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		return http
-				.cors(cors -> cors.disable())
-				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests((auth) -> auth
-						.requestMatchers("/api/auth/**", "/api/customers/**").permitAll()
-						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-						.requestMatchers("/api/admin/**").hasRole("ADMIN")
-						.anyRequest().authenticated()
-				)
-	            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-	            .build();
+		return http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests((auth) -> auth.requestMatchers("/api/auth/**", "/api/customers/**").permitAll()
+						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().requestMatchers("/api/admin/**")
+						.hasRole("ADMIN").anyRequest().authenticated())
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
 
 	}
 
