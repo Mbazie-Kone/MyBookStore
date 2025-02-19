8a91f65afcc167f0d5972df251b371f2c56c3414
M'bazie Kone <mbazie89@gmail.com>
2025-02-19 01:41:08 +0100

    Updates

--- frontend/src/app/services/user-admin.service.ts
+++ frontend/src/app/services/user-admin.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class UserAdminService {
 
-  constructor() { }
+  private apiLoginUrl = 'http://localhost:8080/api/auth/login';
+
+  private apiRegisterUrl = 'http://localhost:8080/api/auth/register';
+
+  constructor(private http: HttpClient) { }
 }
