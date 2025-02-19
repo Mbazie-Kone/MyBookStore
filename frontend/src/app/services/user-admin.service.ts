6ad929070b9498a85cd526abafc53f7693573371
M'bazie Kone <mbazie89@gmail.com>
2025-02-19 01:43:44 +0100

    Updates

--- frontend/src/app/services/user-admin.service.ts
+++ frontend/src/app/services/user-admin.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +12,20 @@ export class UserAdminService {
   private apiRegisterUrl = 'http://localhost:8080/api/auth/register';
 
   constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    const body = { username, password };
+      
+    return this.http.post<any>(this.apiLoginUrl, body, {
+      headers: {'Content-Type': 'application/json'}
+    });
+  }
+
+  register(username: string, password:string, role: string): Observable<{ message: string; role: string}> {
+    const body = {username, password, role};
+  
+    return this.http.post<{ message: string; role: string}>(this.apiRegisterUrl, body, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    });
+  }
 }
