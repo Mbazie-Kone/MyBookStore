8d79d3e92345ed97c7a8253a94e44be3af2cb161
M'bazie Kone <mbazie89@gmail.com>
2025-02-19 01:10:06 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/SecurityConfig.java
@@ -44,19 +44,24 @@ public class SecurityConfig {
 
 	}
 	
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-		configuration.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		
-		return source;
-	}
+	
+	  @Bean 
+	  public CorsConfigurationSource corsConfigurationSource() {
+		  
+		  CorsConfiguration configuration = new CorsConfiguration();
+		  configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+		  configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
+		  configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); 
+		  configuration.setAllowCredentials(true);
+		  
+		  UrlBasedCorsConfigurationSource source = new
+		  UrlBasedCorsConfigurationSource(); source.registerCorsConfiguration("/**",
+		  configuration);
+		  
+		  return source; 
+	 
+	  }
+	 
 
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
