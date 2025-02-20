6d227246c1d1fea85b68045075d3d1999d75fad3
M'bazie Kone <mbazie89@gmail.com>
2025-02-20 19:21:50 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/model/CustomerAddress.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/model/CustomerAddress.java
@@ -1,42 +1,37 @@
-package com.mbaziekone.user_service.model;
+package com.mbaziekone.customer_service.model;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "customers")
+@Table(name = "customer_addresses")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Customer implements Serializable {
+public class CustomerAddress implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; //Removed @GeneratedValue, as the database will handle auto-increment
-
-	private String firstName;
-
-	private String lastName;
-
-	private String email;
+	private Long id;
 	
-	private String username;
-
-	private String password;
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
 	
-	private String phone;
-
-	private LocalDateTime createdAt = LocalDateTime.now();
+	@ManyToOne
+	@JoinColumn(name = "address_id")
+	private Address address;
 
 }
