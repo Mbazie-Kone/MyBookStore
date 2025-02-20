bb5e311d3162a48bb532851d273b69c5677371b5
M'bazie Kone <mbazie89@gmail.com>
2025-02-20 20:17:58 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/repository/CustomerAddressRepository.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/repository/CustomerAddressRepository.java
@@ -1,8 +1,8 @@
-package com.mbaziekone.user_service.repository;
+package com.mbaziekone.customer_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mbaziekone.user_service.model.CustomerAddress;
+import com.mbaziekone.customer_service.model.CustomerAddress;
 
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
 
--- customer-service/src/main/java/com/mbaziekone/customer_service/service/CustomerService.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/service/CustomerService.java
@@ -1,5 +1,17 @@
 package com.mbaziekone.customer_service.service;
 
-public class CustomerService {
+import org.springframework.stereotype.Service;
 
+import com.mbaziekone.customer_service.repository.CustomerAddressRepository;
+import com.mbaziekone.customer_service.repository.CustomerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+	
+	private final CustomerRepository customerRepository;
+	private final AddressRepository addressRepository;
+	private final CustomerAddressRepository customerAddressRepository;
 }
