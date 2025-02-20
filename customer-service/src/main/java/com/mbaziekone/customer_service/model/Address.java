0fd3fa6b93ca25e5fce503a47d4aec58add4f98b
M'bazie Kone <mbazie89@gmail.com>
2025-02-20 19:13:24 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/model/Address.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/model/Address.java
@@ -3,6 +3,9 @@ package com.mbaziekone.customer_service.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,5 +17,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address implements Serializable {
-
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
