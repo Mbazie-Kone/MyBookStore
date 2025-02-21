7fa7bcec7a1e5a7c4ca42246b2858dc94441bc1a
M'bazie Kone <mbazie89@gmail.com>
2025-02-21 23:27:37 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/repository/CustomerAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mbaziekone.customer_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mbaziekone.customer_service.model.CustomerAddress;
-
-public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Long> {
-
-}
--- customer-service/src/main/java/com/mbaziekone/customer_service/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.mbaziekone.customer_service.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import com.mbaziekone.customer_service.model.Customer;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	
 	Optional<Customer> findByEmail(String email);
+	List<Customer> findByCountryId(Long countryId);
+	List<Customer> findByAddressId(Long addressId);
 }
