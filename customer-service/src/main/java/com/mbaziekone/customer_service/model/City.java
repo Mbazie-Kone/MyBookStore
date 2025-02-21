a6a2f20423b268a54c1a1f99ca7b11891e7798b5
M'bazie Kone <mbazie89@gmail.com>
2025-02-21 22:53:16 +0100

    Updates

---
+++ customer-service/src/main/java/com/mbaziekone/customer_service/model/City.java
@@ -0,0 +1,26 @@
+package com.mbaziekone.customer_service.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cities")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class City implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+}
