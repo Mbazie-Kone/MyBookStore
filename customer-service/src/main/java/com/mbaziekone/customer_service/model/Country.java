3c19af6cbfffac64a1091d22f55fd3517e71b759
M'bazie Kone <mbazie89@gmail.com>
2025-02-21 22:18:35 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/model/Country.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/model/Country.java
@@ -3,6 +3,9 @@ package com.mbaziekone.customer_service.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +20,8 @@ public class Country implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
