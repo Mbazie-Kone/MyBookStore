27259ccb637e2daf3b6d5a1aa7d0958c0bfc652b
M'bazie Kone <mbazie89@gmail.com>
2025-02-21 22:41:14 +0100

    Updates

--- customer-service/src/main/java/com/mbaziekone/customer_service/model/State.java
+++ customer-service/src/main/java/com/mbaziekone/customer_service/model/State.java
@@ -1,6 +1,7 @@
 package com.mbaziekone.customer_service.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,4 +34,18 @@ public class State implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "country_id")
 	private Country country;
+	
+	private LocalDateTime createdAt;
+	private LocalDateTime updateAt;
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updateAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updateAt = LocalDateTime.now();
+	}
 }
