9e623dfb7d41096167cdf38c4e2fc14d6b66dcb1
M'bazie Kone <mbazie89@gmail.com>
2025-02-21 23:56:17 +0100

    Updates

---
+++ customer-service/src/main/java/com/mbaziekone/customer_service/service/AddressService.java
@@ -0,0 +1,48 @@
+package com.mbaziekone.customer_service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.mbaziekone.customer_service.model.Address;
+import com.mbaziekone.customer_service.model.City;
+import com.mbaziekone.customer_service.repository.AddressRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AddressService {
+	
+	private final AddressRepository addressRepository;
+	
+	// Find all addresses
+	public List<Address> getAllAddresses() {
+		
+		return addressRepository.findAll();
+	}
+	
+	// Find address by city ID
+	public List<Address> getAddressesByCity(Long cityId) {
+		
+		return addressRepository.findByCityId(cityId);
+	}
+	
+	// Find address by ID
+	public Optional<Address> getAddressById(Long id) {
+		
+		return addressRepository.findById(id);
+	}
+	
+	// Create
+	public Address createAddress(Address address) {
+		
+		return addressRepository.save(address);
+	}
+	
+	// Delete
+	public void deleteAddress(Long id) {
+		addressRepository.deleteById(id);
+	}
+}
