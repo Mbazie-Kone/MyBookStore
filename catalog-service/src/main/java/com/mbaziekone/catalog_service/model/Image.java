f9e71eecdbb601c3522792214d88a185a2659854
M'bazie Kone <mbazie89@gmail.com>
2025-02-22 01:23:35 +0100

    Updates

---
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Image.java
@@ -0,0 +1,26 @@
+package com.mbaziekone.catalog_service.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+}
