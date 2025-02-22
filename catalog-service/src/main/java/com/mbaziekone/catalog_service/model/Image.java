a1245f65d14af75d911da6ab92b35fad81e118bf
M'bazie Kone <mbazie89@gmail.com>
2025-02-22 01:26:12 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Image.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Image.java
@@ -1,11 +1,16 @@
 package com.mbaziekone.catalog_service.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,4 +28,24 @@ public class Image implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	private String imageUrl;
+	
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
 }
