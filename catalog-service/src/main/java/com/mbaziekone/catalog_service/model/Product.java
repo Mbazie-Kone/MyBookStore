da23302d61e9eb851ef7168c256ad463c0da8598
M'bazie Kone <mbazie89@gmail.com>
2025-02-22 01:17:40 +0100

    Updates

---
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Product.java
@@ -0,0 +1,28 @@
+package com.mbaziekone.catalog_service.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+}
