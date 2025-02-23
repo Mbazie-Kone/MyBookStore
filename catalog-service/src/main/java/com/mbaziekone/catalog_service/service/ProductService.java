baddf8022b97254fa30ea38e9244c9ed08156dc7
M'bazie Kone <mbazie89@gmail.com>
2025-02-23 22:14:11 +0100

    Updates

---
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.mbaziekone.catalog_service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.mbaziekone.catalog_service.model.Product;
+import com.mbaziekone.catalog_service.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+	
+	private final ProductRepository productRepository;
+	
+	public List<Product> getAllProducts() {
+		
+		return productRepository.findAll();
+	}
+	
+	public Optional<Product> getProductById(Long id) {
+		
+		return productRepository.findById(id);
+	}
+}
