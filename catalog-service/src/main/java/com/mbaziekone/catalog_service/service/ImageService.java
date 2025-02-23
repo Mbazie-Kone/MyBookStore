eaf990c31fd7266c18b00a9001fb91927bb39bed
M'bazie Kone <mbazie89@gmail.com>
2025-02-23 22:24:16 +0100

    Updates

---
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/service/ImageService.java
@@ -0,0 +1,42 @@
+package com.mbaziekone.catalog_service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.mbaziekone.catalog_service.model.Image;
+import com.mbaziekone.catalog_service.repository.ImageRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+	
+	private final ImageRepository imageRepository;
+	
+	public List<Image> getAllImages() {
+		
+		return imageRepository.findAll();
+	}
+	
+	public List<Image> getImagesByProduct(Long productId) {
+		
+		return imageRepository.findByProductId(productId);
+	}
+	
+	public Optional<Image> getImageById(Long id) {
+		
+		return imageRepository.findById(id);
+	}
+
+	public Image createImage(Image image) {
+		
+		return imageRepository.save(image);
+	}
+	
+	public void deleteImage(Long id) {
+		imageRepository.deleteById(id);
+	}
+}
