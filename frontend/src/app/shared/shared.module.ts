c8b7b6e04ad997721efd61169e7cbe061427e654
M'bazie Kone <mbazie89@gmail.com>
2025-02-24 05:35:54 +0100

    Updates

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent,
+    CardComponent,
+    ModalComponent
+  ],
   imports: [
     CommonModule
   ]
