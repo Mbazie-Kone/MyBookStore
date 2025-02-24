1cfba86f5cf0a144ab7336504da0b15563c4cb6d
M'bazie Kone <mbazie89@gmail.com>
2025-02-24 05:44:05 +0100

    Updates

--- frontend/src/app/shared/components/card/card.component.ts
+++ frontend/src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  @Input() image: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
 }
