8f8995c05fbea0ed5ce89d65b651c1408e4e0801
M'bazie Kone <mbazie89@gmail.com>
2025-02-25 02:49:35 +0100

    Updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,22 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductsComponent } from './admin/products/products.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
