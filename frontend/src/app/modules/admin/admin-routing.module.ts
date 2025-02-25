1c76cd766163a521e3c8db88690ad32d7a8fe6df
M'bazie Kone <mbazie89@gmail.com>
2025-02-25 03:24:29 +0100

    Updates

--- frontend/src/app/modules/admin/admin-routing.module.ts
+++ frontend/src/app/modules/admin/admin-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { authGuard } from '../../guards/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { authGuard } from '../../guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
   { path: 'products', component: ProductsComponent}
 ];
 
