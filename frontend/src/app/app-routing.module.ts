caf2754079f8c362ba7c6992f6c9d825bb96e2b4
M'bazie Kone <mbazie89@gmail.com>
2025-02-28 00:29:30 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { LoginComponent } from './modules/admin/auth/login/login.component';
 import { RegisterComponent } from './modules/admin/auth/register/register.component';
 import { DashboardComponent } from './modules/admin/pages/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { ProductsComponent } from './modules/admin/pages/products/products.component';
 
 const routes: Routes = [
   { path: 'admin/login', component: LoginComponent },
   { path: 'admin/register', component: RegisterComponent },
   { path: 'admin/dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'admin/products', component: ProductsComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path:'home', component: LayoutComponent,
