abc8cd938630ee832353f16bd0ea986b5b077dd8
M'bazie Kone <mbazie89@gmail.com>
2025-02-28 00:22:56 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { ErrorComponent } from './errors/error/error.component';
+import { LoginComponent } from './modules/admin/auth/login/login.component';
 
 const routes: Routes = [
+  { path: 'admin/login', component: LoginComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path:'home', component: LayoutComponent,
