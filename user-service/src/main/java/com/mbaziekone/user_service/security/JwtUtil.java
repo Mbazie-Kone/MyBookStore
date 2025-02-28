ac18d67a8a0df363c1385d4623f20a2287237d04
M'bazie Kone <mbazie89@gmail.com>
2025-02-28 05:03:55 +0100

    Updates

--- user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
+++ user-service/src/main/java/com/mbaziekone/user_service/security/JwtUtil.java
@@ -7,6 +7,8 @@ import java.util.function.Function;
 
 import org.springframework.stereotype.Component;
 
+import com.mbaziekone.user_service.service.UserService;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -17,6 +19,12 @@ public class JwtUtil {
 	private final String SECRET_KEY = "Y29tcGxleFNlY3JldEtleU9mQW1pbmltdW1MZW5ndGhGQW5kU2FmZUFsd2F5cw==";
 	private final long EXPIRATION_TIME = 1000 * 60 * 60; //1 hour
 	private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+	
+	private final UserService userService;
+	
+	public JwtUtil(UserService userService) {
+		this.userService = userService;
+	}
 
 	public String generateToken(String username) {
 			
