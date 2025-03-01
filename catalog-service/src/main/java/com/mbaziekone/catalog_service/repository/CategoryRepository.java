cf01118bcdeb00565de49845fb3d308da0325d67
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 11:51:31 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/CategoryRepository.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.mbaziekone.catalog_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.mbaziekone.catalog_service.model.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+	Optional<Category> findByName(String name);
 }
