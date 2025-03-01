5886d9c43a3510f5cdb4e0f9c98e1f5df7165a98
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 08:07:46 +0100

    Updates

--- frontend/src/app/modules/admin/auth/login/login.component.ts
+++ frontend/src/app/modules/admin/auth/login/login.component.ts
@@ -24,6 +24,8 @@ export class LoginComponent {
 
         const decodedToken: any = jwtDecode(response.token);
         console.log(decodedToken);
+        console.log("Role in the token:", decodedToken.role);
+        console.log("match with'ADMIN':", decodedToken.role === "ADMIN");
 
         console.log("LocalStorage token:", localStorage.getItem('token'));
 
