3a8803413f489c6dcef9d0a0f45638b9711766b5
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 11:56:03 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
@@ -3,8 +3,18 @@ package com.mbaziekone.catalog_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mbaziekone.catalog_service.repository.CategoryRepository;
+import com.mbaziekone.catalog_service.repository.ImageRepository;
+import com.mbaziekone.catalog_service.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/catalog")
+@RequiredArgsConstructor
 public class CatalogController {
-
+	
+	private final ProductRepository productRepository;
+	private final CategoryRepository categoryRepository;
+	private final ImageRepository imageRepository;
 }
