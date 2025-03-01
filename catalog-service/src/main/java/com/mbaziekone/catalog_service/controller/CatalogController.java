659ccd361fb958980cd28d812754f1ddf683c9a2
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 11:58:16 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
@@ -1,8 +1,11 @@
 package com.mbaziekone.catalog_service.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mbaziekone.catalog_service.dto.InsertCategoryProductImage;
 import com.mbaziekone.catalog_service.repository.CategoryRepository;
 import com.mbaziekone.catalog_service.repository.ImageRepository;
 import com.mbaziekone.catalog_service.repository.ProductRepository;
@@ -17,4 +20,8 @@ public class CatalogController {
 	private final ProductRepository productRepository;
 	private final CategoryRepository categoryRepository;
 	private final ImageRepository imageRepository;
+	
+	public ResponseEntity<Product> addProduct(@RequestBody InsertCategoryProductImage dto) {
+		
+	}
 }
