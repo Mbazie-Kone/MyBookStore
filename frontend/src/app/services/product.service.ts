57f3240f8ded9b796044fb58821e992264491484
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 12:53:16 +0100

    Updates

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private addProductUrl = 'http://localhost:8080/api/catalog/insert/product'
+  private addProductUrl = 'http://localhost:8080/api/catalog/insert/product';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //Image
+  uploadImage(file: File): Observable<string> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(`${this.addProductUrl}/upload-image`, formData, { responseType: 'text'});
+  }
 }
