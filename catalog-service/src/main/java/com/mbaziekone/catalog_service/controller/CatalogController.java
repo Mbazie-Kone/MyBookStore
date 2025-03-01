c361df6257112b7eecb346530626fb26437ba24c
M'bazie Kone <mbazie89@gmail.com>
2025-03-01 12:08:18 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
@@ -1,11 +1,16 @@
 package com.mbaziekone.catalog_service.controller;
 
+import java.util.function.LongToDoubleFunction;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mbaziekone.catalog_service.dto.InsertCategoryProductImage;
+import com.mbaziekone.catalog_service.model.Category;
+import com.mbaziekone.catalog_service.model.Image;
+import com.mbaziekone.catalog_service.model.Product;
 import com.mbaziekone.catalog_service.repository.CategoryRepository;
 import com.mbaziekone.catalog_service.repository.ImageRepository;
 import com.mbaziekone.catalog_service.repository.ProductRepository;
@@ -22,6 +27,31 @@ public class CatalogController {
 	private final ImageRepository imageRepository;
 	
 	public ResponseEntity<Product> addProduct(@RequestBody InsertCategoryProductImage dto) {
+		// Category
+		Category category = categoryRepository.findByName(dto.getCategoryName()).orElseGet(() -> {
+			Category newCategory = new Category();
+			newCategory.setName(dto.getCategoryName());
+			newCategory.setDescription(dto.getCategoryDescription());
+			
+			return categoryRepository.save(newCategory);
+		});
 		
+		// Image
+		Image image = new Image();
+		image.setImageUrl(dto.getImageUrl());
+		
+		imageRepository.save(image);
+		
+		// Product
+		Product product = new Product();
+		product.setName(dto.getName());
+		product.setDescription(dto.getDescription());
+		product.setPrice(dto.getPrice());
+		product.setStock(dto.getStock());
+		product.setAvailable(dto.isAvailable());
+		product.setCategory(category);
+		
+		productRepository.save(product);
+	
 	}
 }
