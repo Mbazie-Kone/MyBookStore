33a6edc3a79656e02d53995ed2ee6e07bb9ba10b
M'bazie Kone <mbazie89@gmail.com>
2025-03-02 07:38:55 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Product.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/model/Product.java
@@ -34,7 +34,7 @@ public class Product implements Serializable {
 	private String description;
 	private BigDecimal price;
 	private int stock;
-	private boolean isAvailable = true;
+	private boolean isAvailable;
 	
 	@ManyToOne
 	@JoinColumn(name = "category_id")
