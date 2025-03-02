8adf41a2e51b8b2b63bfba07c56ebea724e8a5e5
M'bazie Kone <mbazie89@gmail.com>
2025-03-02 21:44:06 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/ImageRepository.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/ImageRepository.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mbaziekone.catalog_service.model.Image;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+	
 	List<Image> findByProductId(Long productId);
+	
+	@Transactional
+	void deleteAllByProductId(Long productId);
 }
