44356e8b3b755b524a6847b9e1cf864430db3209
M'bazie Kone <mbazie89@gmail.com>
2025-03-05 01:27:12 +0100

    Updates

--- frontend/src/app/modules/admin/pages/update-product/update-product.component.ts
+++ frontend/src/app/modules/admin/pages/update-product/update-product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-product',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './update-product.component.html',
   styleUrl: './update-product.component.css'
 })
-export class UpdateProductComponent {
+export class UpdateProductComponent implements OnInit {
 
+  product = {
+    name: '',
+    description: '',
+    price: 0,
+    stock: 0,
+    isAvailable: true,
+    categoryName: '',
+    imageUrl: ''
+  };
+
+  ngOnInit(): void {
+    
+  }
 }
