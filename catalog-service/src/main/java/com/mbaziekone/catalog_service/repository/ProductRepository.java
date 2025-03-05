7779eaa9bae1346dff9f5b583c1aa1b71f0b1875
M'bazie Kone <mbazie89@gmail.com>
2025-03-05 01:54:48 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/ProductRepository.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.mbaziekone.catalog_service.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import com.mbaziekone.catalog_service.model.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findByCategoryId(Long categoryId);
 	List<Product> findByIsAvailableTrue();
+	Optional<Product> findByName(String name);
 }
