5b775f892d525adc3e189044531437fb76f2544e
M'bazie Kone <mbazie89@gmail.com>
2025-03-06 20:57:50 +0100

    Updates

--- frontend/src/app/modules/catalog/pages/product-details/product-details.component.ts
+++ frontend/src/app/modules/catalog/pages/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../../models/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  product: Product | null = null;
+  errorMessage: string = '';
 
+  constructor(private route: ActivatedRoute, private productService: ProductService) {}
 }
