62484665335bfbba2263da9aa979d86cd68689c3
M'bazie Kone <mbazie89@gmail.com>
2025-03-06 20:33:58 +0100

    Updates

--- frontend/src/app/modules/catalog/catalog.module.ts
+++ frontend/src/app/modules/catalog/catalog.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
-    CatalogComponent
+    CatalogComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
