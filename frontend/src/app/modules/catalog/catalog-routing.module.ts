3a8e28a37de7e5ffaa91c00bab45dd4c08f56aa0
M'bazie Kone <mbazie89@gmail.com>
2025-03-06 20:50:15 +0100

    Updates

--- frontend/src/app/modules/catalog/catalog-routing.module.ts
+++ frontend/src/app/modules/catalog/catalog-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 const routes: Routes = [
-  { path: '', component: CatalogComponent}
+  { path: '', component: CatalogComponent},
+  { path: 'product-details/:id', component: ProductDetailsComponent}
 ];
 
 @NgModule({
