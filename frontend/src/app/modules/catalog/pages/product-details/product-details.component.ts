42448baf0dd3dc662116100ff859a8d416679ea3
M'bazie Kone <mbazie89@gmail.com>
2025-03-08 09:42:53 +0100

    Updates

--- frontend/src/app/modules/catalog/pages/product-details/product-details.component.ts
+++ frontend/src/app/modules/catalog/pages/product-details/product-details.component.ts
@@ -42,7 +42,7 @@ export class ProductDetailsComponent implements OnInit {
 
         // Check if there are images and set the first one as selected
         if (this.product.imageUrls.length > 0) {
-          this.selectedImage = 'http://localhost:4200' + this.product.imageUrls[0];
+          this.selectedImage = this.product.imageUrls.length > 0 ? this.product.imageUrls[0] : '';
         } else {
           console.warn("No images found for this product.");
         }
