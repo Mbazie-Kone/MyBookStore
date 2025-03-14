910725608b28e8e043824255305ca2eb5c9bdca9
M'bazie Kone <mbazie89@gmail.com>
2025-03-14 13:02:14 +0100

    Updates

--- catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
+++ catalog-service/src/main/java/com/mbaziekone/catalog_service/controller/CatalogController.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
