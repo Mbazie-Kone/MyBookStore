6f63e2eee98ce9bde2b306241a2ed8dbe8c216ca
M'bazie Kone <mbazie89@gmail.com>
2025-03-22 20:24:58 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,22 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { ErrorComponent } from './errors/error/error.component';
-import { LoginComponent } from './modules/admin/auth/login/login.component';
-import { RegisterComponent } from './modules/admin/auth/register/register.component';
-import { DashboardComponent } from './modules/admin/pages/dashboard/dashboard.component';
-import { authGuard } from './guards/auth.guard';
-import { ProductsComponent } from './modules/admin/pages/products/products.component';
-import { ViewProductsComponent } from './modules/admin/pages/view-products/view-products.component';
-import { UpdateProductComponent } from './modules/admin/pages/update-product/update-product.component';
 import { AdminLayoutComponent } from './core/components/admin/admin-layout/admin-layout.component';
 
 const routes: Routes = [
   {
-    path: 'admin/',
+    path: 'admin',
     component: AdminLayoutComponent,
     children: [
-      { path: '', redirectTo: 'admin', pathMatch: 'full'},
-      { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }
+      { path: 'admin', redirectTo: 'admin/login', pathMatch: 'full'},
+      { path: 'admin/login', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }
     ]
   },
   {
